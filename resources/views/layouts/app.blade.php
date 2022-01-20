dd73032ec29f6aea5e7dc724718d8387466ffcbb
JordHemsworth <jordanhemsworth+1@gmail.com>
2022-01-20 13:09:54 +0000

    First Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="contain mx-auto flex items-center justify-between px-4 py-y">
+
+            <div class="flex items-centre">
+                <a href="/">
+                    <img src="/" alt="logo" class="w-32 flex-none">
+                </a>
+
+                <ul class="flex ml-16 space-x-8">
+                    <li><a href="#" class="hover:text-gray-400"> Games </a></li>
+                    <li><a href="#" class="hover:text-gray-400"> Reviews </a></li>
+                    <li><a href="#" class="hover:text-gray-400"> Coming Soon </a></li>
+                </ul>
+            </div>
+
+            <div class="flex items-center">
+                <div class="relative">  
+                    <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-3 py-1" placeholder="Search...">             {{-- TODO add search icon svg --}}
+                    
+                </div>
+
+                <div class="ml-6">
+                    <a href="#"><img src="/avatar.jpg" alt="avatar" class="rounded-full w-8">
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered By <a href="#" class="underline hover:text-gray-400"> IGDB API</a>
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
